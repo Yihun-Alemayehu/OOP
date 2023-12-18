75e18efc53d07eb969901cb0ff0e9fa6975911f4
Yihun Alemayehu <yankure01@gmail.com>
2023-12-18 16:17:44 +0300

    idk commit

---
+++ idk.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class idk {
+    public static void main(String args[]) {
+        int numDynamic;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter whatever you want ?");
+        numDynamic = input.nextInt();
+        System.out.print(numDynamic);
+    }
+}
