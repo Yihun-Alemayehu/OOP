168c298b34f1c0f47b52224d8c8ee97d034510b9
Yihun Alemayehu <yankure01@gmail.com>
2023-12-18 16:51:40 +0300

    fdb commit

--- idk.java
+++ idk.java
@@ -2,10 +2,19 @@ import java.util.Scanner;
 
 public class idk {
     public static void main(String args[]) {
-        int numDynamic;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter whatever you want ?");
-        numDynamic = input.nextInt();
-        System.out.print(numDynamic);
+        // int numDynamic;
+        // Scanner input = new Scanner(System.in);
+        // System.out.println("Enter whatever you want ?");
+        // numDynamic = input.nextInt();
+        // System.out.print(numDynamic);
+
+        // byte a = 64, b;
+        // int i = a << 2;
+        // b = (byte) (a << 2);
+        // System.out.println(i);
+        // System.out.println(b);
+
+        int a = -16;
+        System.out.println(a >> 3);
     }
 }
